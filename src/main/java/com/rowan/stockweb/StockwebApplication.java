99591c674d15e82b151b8eeca6987ec6aecbc8f9
rowanfoo <rowanfoo@gmail.com>
2018-07-03 23:16:40 +0800

    This is for tomcat

--- src/main/java/com/rowan/stockweb/StockwebApplication.java
+++ src/main/java/com/rowan/stockweb/StockwebApplication.java
@@ -1,10 +1,13 @@
 package com.rowan.stockweb;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
+
+
+public class StockwebApplication {
+}
+
+
+/*
 @SpringBootApplication
 public class StockwebApplication {
     @Bean
@@ -17,3 +20,5 @@ public class StockwebApplication {
         SpringApplication.run(StockwebApplication.class, args);
     }
 }
+
+*/
